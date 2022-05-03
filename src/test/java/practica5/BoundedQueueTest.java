8dd5f3dcc16f2081f5259b85e34928acd0c8c8aa
sergioM <sermorepe@gmail.com>
2022-05-03 11:56:06 +0200

    Muchos test hechos

--- src/test/java/practica5/BoundedQueueTest.java
+++ src/test/java/practica5/BoundedQueueTest.java
@@ -12,16 +12,15 @@ public class BoundedQueueTest {
     /**
      * Casos de Prueba
      * <p>
-     * 1- Comprobar una creacion correcta
-     * 2- Comprobar que se añade
-     * 3- Comprobar que se coge
-     * 4- Comprobar que esta Completa
-     * 5- Comprobar que esta vacio
-     * 6- Comprobar el numero de objetos
-     * 7- Lanza Excepción si se quiere añadir un objeto y lista llena
-     * 8- Lanza Excepción si se quiere coger un objeto y lista vacia
+     * 1- Comprobar una creacion correcta                               X
+     * 2- Comprobar que se añade                                        X
+     * 3- Comprobar que se coge                                         X
+     * 4- Comprobar que esta Completa                                   X
+     * 5- Comprobar que esta vacio                                      X
+     * 6- Comprobar el numero de objetos                                X
+     * 7- Lanza Excepción si se quiere añadir un objeto y lista llena   X
+     * 8- Lanza Excepción si se quiere coger un objeto y lista vacia    X
      * 9-
-     *
      */
 
 
@@ -37,19 +36,44 @@ public class BoundedQueueTest {
         var numberOfItems = (int) ReflectionTestUtils.getField(bondedQueue, "numberOfItems");
 
         assertThat(capacity).isEqualTo(EXPECTED_CAPACITY);
-        assertThat(nextItem).isEqualTo(nextFreeSlot).isEqualTo(numberOfItems)
-                .isEqualTo(0);
+        assertThat(nextItem).isEqualTo(nextFreeSlot).isEqualTo(numberOfItems).isEqualTo(0);
         assertThat(buffer).isEmpty();
     }
 
-    @Test
-    public void shouldCreateABoundedQueueCorrectly() {
+
+    @Test//2- Comprobar que se añade && 3- Comprobar que se coge
+    public void shouldAddCorrectElement() {
 
         var boundedQueue = new BoundedQueue<Integer>(1);
         boundedQueue.put(1);
-        System.out.println(boundedQueue.get());
         assertThat(boundedQueue.get()).isEqualTo(1);
 
+    }
+
+    @Test//4- Comprobar que esta Completa
+    public void QueueShouldBeFull() {
+        var boundedQueue = new BoundedQueue<Integer>(3);
+        boundedQueue.put(1);
+        boundedQueue.put(1);
+        boundedQueue.put(1);
+
+        assertThat(boundedQueue.isFull()).isEqualTo(true);
+    }
+
+    @Test//5- Comprobar que esta vacio
+    public void QueueShouldBeEmpty() {
+        var boundedQueue = new BoundedQueue<Integer>(3);
+        assertThat(boundedQueue.isEmpty()).isEqualTo(true);
+    }
+
+    @Test//6- Comprobar el numero de objetos
+    public void shouldReturnTheCorrectNumberOfItems() {
+        var boundedQueue = new BoundedQueue<Integer>(5);
+        boundedQueue.put(1);
+        boundedQueue.put(1);
+        boundedQueue.put(1);
+
+        assertThat(boundedQueue.getNumberOfItems()).isEqualTo(3);
     }
 
     @Test//7- Lanza Excepción si se quiere añadir un objeto y lista llena
